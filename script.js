ec72d9a0920ac28fc22a37c9aaf94722e18edcb9
Tamtam19 <t.gogichaishvilii@gmail.com>
2022-05-16 17:16:49 +0200

    lessons 1

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let numbers = ['giorgi, levani, nodari', 2, 200,489];
+// დავალება 1
+let  user = {
+    firstname:'giorgi',
+    lastname: 'smith',
+    age: 25,
+    studentstatus: 'active'
+    };
+    console.log(user.studentstatus);
+
+// დავალება 2
+
+    let numbers = ['giorgi, levani, nodari', 2, 200,489];
 
 for (let item = 0; item < numbers.length; item++) {
-    console.log(numbers[item]);
+ console.log(numbers[item]);
 }
  
