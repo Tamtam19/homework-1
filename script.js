605ccec4c252e4de9af74827da5d265ddf29b42e
Tamtam19 <t.gogichaishvilii@gmail.com>
2022-05-16 17:32:43 +0200

    lesson1

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// დავალება 1
+let  user = {
+    firstname:'giorgi',
+    lastname: 'smith',
+    age: 25,
+    studentstatus: 'active'
+    };
+    console.log(user.studentstatus);
 
+// დავალება 2
+
+    let numbers = ['giorgi, levani, nodari', 2, 200,489];
+
+for (let item = 0; item < numbers.length; item++) {
+ console.log(numbers[item]);
+}
 
 //დავალება 3
 let mas=[12,2,34,5,6,2,34,56,9,5,1,2,54];
