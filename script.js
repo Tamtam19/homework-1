4cb2d978301a805338b827ef71155b01a046c933
Tamtam19 <t.gogichaishvilii@gmail.com>
2022-05-16 17:31:41 +0200

    lesson1

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// დავალება 1
-let  user = {
-    firstname:'giorgi',
-    lastname: 'smith',
-    age: 25,
-    studentstatus: 'active'
-    };
-    console.log(user.studentstatus);
 
-// დავალება 2
 
-    let numbers = ['giorgi, levani, nodari', 2, 200,489];
-
-for (let item = 0; item < numbers.length; item++) {
- console.log(numbers[item]);
+//დავალება 3
+let mas=[12,2,34,5,6,2,34,56,9,5,1,2,54];
+for(let i = 0; i < mas.length; i++) {
+    if(mas[i]>5)
+    console.log(mas[i]);
 }
- 
